60094fe501cd4ece26ea034bcf9c242e5e76882c
user.email <jobayer.ahmed109@gmail.com>
2023-12-31 13:47:28 +0600

    User registration and password hashing done

--- index.js
+++ index.js
@@ -3,8 +3,43 @@ const connectDB = require("./db/db");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const User = require("./models/User");
+const bcrypt = require("bcrypt");
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.status(200).send("<h1>Attendance server is running</h1>");
+});
+
+// Register
+app.post("/register", async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "User already exist" });
+    }
+
+    user = new User({ name, email, password });
+    // password hashing
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+    user.password = hash;
+
+    await user.save();
+
+    res.status(201).json({ message: "User created successfully", user });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Global error handle.
+app.use((err, req, res, next) => {
+  next(err.message);
+  return res.status(500).json({ message: "Server error occurred" });
 });
 
 // Connect Database
