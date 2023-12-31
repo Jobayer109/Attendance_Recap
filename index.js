46b93ab49477465dd257a608f1b6d33359b18448
user.email <jobayer.ahmed109@gmail.com>
2023-12-31 12:14:28 +0600

    database connection done

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const connectDB = require("./db/db");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -6,6 +7,15 @@ app.get("/", (req, res) => {
   res.status(200).send("<h1>Attendance server is running</h1>");
 });
 
-app.listen(port, () => {
-  console.log(`Server is listening on port: http://localhost:${port}`);
-});
+// Connect Database
+connectDB("mongodb://127.0.0.1:27017/Attendance_Recap")
+  .then(() => {
+    console.log("Database connected");
+
+    app.listen(port, () => {
+      console.log(`Server is listening on port: http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
